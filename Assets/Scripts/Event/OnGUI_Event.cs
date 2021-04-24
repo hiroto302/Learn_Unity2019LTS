f1dfc14cb1c2d413961a59719ac47cfd945793fa
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-25 02:20:08 +0900

    OnGUI

---
+++ Assets/Scripts/Event/OnGUI_Event.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnGUI_Event : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // You can only call GUI functions from inside OnGUI.
+        // GUI.Button(new Rect(10, 10, 150, 100), "I am a button");
+    }
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 10, 150, 100), "I am a button"))
+        {
+            print("You clicked the button!");
+        }
+    }
+}
