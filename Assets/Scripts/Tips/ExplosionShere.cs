6679b7a5bcc60a140aed7db077a53dbd228e1e58
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-25 16:04:10 +0900

    Fractured Object 作成

---
+++ Assets/Scripts/Tips/ExplosionShere.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionShere : MonoBehaviour
+{
+    [SerializeField] GameObject fracturedObject;
+
+    void OnMouseDown()
+    {
+        Explosion();
+    }
+    void Explosion()
+    {
+        Instantiate(fracturedObject, transform.position, Quaternion.identity);
+        Destroy(this.gameObject);
+    }
+
+}
---
+++ Assets/Scripts/Tips/FracturedObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FracturedObject : MonoBehaviour
+{
+    void Start()
+    {
+        Rigidbody[] allRigidbodies = GetComponentsInChildren<Rigidbody>();
+        foreach(var brokenPiece in allRigidbodies)
+        {
+            brokenPiece.AddExplosionForce(300,transform.position, 1);
+        }
+    }
+}
