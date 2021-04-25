2f9e48de695291a88e8f1d19cdcaa9969671f272
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-25 16:40:03 +0900

    TimeScale

---
+++ Assets/Scripts/Tips/TimeScale.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class TimeScale : MonoBehaviour
+{
+    public Button button;
+
+    void Start()
+    {
+        button.onClick.AddListener(SlowMotion);
+    }
+    public void SlowMotion()
+    {
+        Time.timeScale = 0.2f;
+    }
+    public void SlowMotion(float timeScale)
+    {
+        Time.timeScale = timeScale;
+    }
+}
---
+++ Assets/Scripts/Tips/TimeScaleManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class SlowTimeEvent : UnityEvent<float>
+{
+}
+public class TimeScaleManager : MonoBehaviour
+{
+    public SlowTimeEvent slowTimeEvent = new SlowTimeEvent();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+            slowTimeEvent.AddListener(SlowMotion);
+    }
+
+    public void InvokeSlowMotion()
+    {
+        slowTimeEvent.Invoke(1);
+    }
+
+    public void SlowMotion(float timeScale)
+    {
+        Time.timeScale = timeScale;
+    }
+}
