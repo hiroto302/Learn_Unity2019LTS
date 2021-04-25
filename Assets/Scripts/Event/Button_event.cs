db3c188a806f45509b63a986c4e532f02c0132b1
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-25 13:20:09 +0900

    OnClick.AddListener

---
+++ Assets/Scripts/Event/Button_event.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class ShowInfoEvent : UnityEvent
+{
+    string name;
+    int age;
+    public ShowInfoEvent(string name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+    public void ShowInfo()
+    {
+        Debug.Log(name + " " + age);
+    }
+}
+// ボタン操作に関する、発生イベントを管理するクラス
+public class Button_event : MonoBehaviour
+{
+    public ShowInfoEvent showInfo1 = new ShowInfoEvent("hiro", 302);
+
+    void Start()
+    {
+        showInfo1.AddListener(showInfo1.ShowInfo);
+    }
+
+    public void InvokeShowInfo1()
+    {
+        showInfo1.Invoke();
+    }
+
+}
---
+++ Assets/Scripts/Event/Button_referenceEvent.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+// ボタン操作の際、対応したイベントを実行するクラス
+public class Button_referenceEvent : MonoBehaviour
+{
+    public Button showInfoButton;
+    // Button_event buttonEvents;
+    ShowInfoEvent showInfoEvent;
+    void Start()
+    {
+        // 参照するイベントのインスタンス
+        /*  MonoBehaviour を 継承してるクラスは参照できない
+            You are trying to create a MonoBehaviour using the 'new' keyword.
+            This is not allowed.  MonoBehaviours can only be added using AddComponent().
+            buttonEvents = new Button_event(); だめ！！
+        */
+        showInfoEvent = new ShowInfoEvent("hiro", 302);
+        // ボタンの取得
+        showInfoButton.GetComponent<Button>();
+        // ボタンが押された時、実行するイベントを加える
+        showInfoButton.onClick.AddListener(showInfoEvent.ShowInfo);
+    }
+
+    void ShowInfo()
+    {
+        Debug.Log("Hello!!");
+    }
+}
