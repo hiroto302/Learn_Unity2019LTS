e89147d6eebd830f566580904aa2b33b048f1401
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-25 21:58:10 +0900

    名前空間, Static Class, UtilityHelper Class/Method 作成・活用方法

---
+++ Assets/Scripts/Classes/Creator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CreateObject; // オブジェクト作成のためのUtilityHelperを提供
+
+public class Creator : MonoBehaviour
+{
+    void Start()
+    {
+        GameObject someObjet =  UtilityHelper.CreateObject(PrimitiveType.Capsule);
+        UtilityHelper.SetPositionZero(someObjet);
+        UtilityHelper.ChangeColor(someObjet, Color.red, true);
+    }
+}
---
+++ Assets/Scripts/Classes/UtilityHelper.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// PrimitiveObjectを作成する機能を提供する
+namespace CreateObject
+{
+    // Static クラスにすることで、static タイプの変数、メソッドしか持っていないことを明示している
+    public static class UtilityHelper
+    {
+        public static GameObject CreateObject(PrimitiveType primitiveType)
+        {
+            return GameObject.CreatePrimitive(primitiveType);
+        }
+        public static void SetPositionZero(GameObject obj)
+        {
+            obj.transform.position = Vector3.zero;
+        }
+        public static void ChangeColor(GameObject obj, Color color, bool randomColor = false)
+        {
+            if(randomColor)
+            {
+                color = new Color(Random.value, Random.value, Random.value);
+            }
+            obj.GetComponent<MeshRenderer>().material.color = color;
+        }
+    }
+}
