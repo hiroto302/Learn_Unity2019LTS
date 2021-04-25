7d24047bc07ed676e087a4dc0f98abba1fa5a739
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-25 21:15:40 +0900

    Static Constructer, 継承, System.Serializable の活用方

---
+++ Assets/Scripts/Classes/StaticConstructor.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Shop
+{
+    public string name; // 店名
+    public int number;  // 号店番号
+}
+
+[System.Serializable]
+public class SAGAShop : Shop
+{
+    // static type は、hierarchy に表示されない
+    public static string locality;
+    public string message;
+    static SAGAShop()
+    {
+        locality = "SAGA";
+    }
+    public SAGAShop(string name, int number, string message)
+    {
+        this.name = name;
+        this.number = number;
+        this.message = message;
+        Debug.Log(locality + " " + number + "号店" + name + message);
+    }
+}
+
+
+// Static の Constructor にテストサンプル
+public class StaticConstructor : MonoBehaviour
+{
+    public SAGAShop karatuShop;
+    void Start()
+    {
+        karatuShop = new SAGAShop("karatu", 1, "できたばい");
+    }
+
+}
