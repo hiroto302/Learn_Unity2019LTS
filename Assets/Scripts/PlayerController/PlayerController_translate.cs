e6391baad48da1463e603093bbcb72faae6cad2b
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-25 19:13:20 +0900

    Update PlayerController_translate.cs

--- Assets/Scripts/PlayerController/PlayerController_translate.cs
+++ Assets/Scripts/PlayerController/PlayerController_translate.cs
@@ -8,23 +8,28 @@ public class PlayerController_translate : MonoBehaviour
     Vector3 _inputVector;
     [SerializeField] float _moveSpeed = 2.0f;
 
+    Vector3 directionToFace;
+
     void Update()
     {
         float hor = Input.GetAxis("Horizontal");
         float ver = Input.GetAxis("Vertical");
         _inputVector = new Vector3(hor, 0, ver);
+
+        Debug.DrawRay(transform.position, directionToFace, Color.blue);
+
         if(_inputVector.sqrMagnitude > 0.1)
         {
-            // 現在位置に移動したい方向を足す
-            Vector3 directionToFace = transform.position + _inputVector.normalized * 10.0f;
-            Debug.Log(directionToFace);
-            // y を 0
-            directionToFace = new Vector3(directionToFace.x, 0, directionToFace.z);
-            Debug.DrawRay(transform.position, directionToFace, Color.blue);
+            // 移動したい方向の位置 : 現在位置から移動したい位置
+            Vector3 directionToFacePosition = transform.position + _inputVector.normalized * 10.0f;
+            // 移動方向 = destination - source
+            directionToFace = directionToFacePosition - transform.position;
+            // 上記での処理は、つまり以下の通りになる。 現在位置から、相対的にどこの方向を向きたいか.
+            directionToFace = _inputVector.normalized * 10;
+            // 回転
             Quaternion directionRotation = Quaternion.LookRotation(directionToFace);
             transform.rotation = Quaternion.Lerp(transform.rotation, directionRotation, Time.deltaTime * 2);
-            // transform.rotation = directionRotation;
-            // transform.LookAt(transform.position + _inputVector.normalized);
+            // 移動
             transform.Translate(transform.forward * _moveSpeed * Time.deltaTime, Space.World);
         }
     }
