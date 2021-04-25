23732ea92056d04e592fe303a34fde7358c5db5c
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-25 19:12:23 +0900

    回転_Aim

---
+++ Assets/Scripts/PlayerController/Aim_Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aim_Player : MonoBehaviour
+{
+    [SerializeField] Transform _target;
+
+    void Update()
+    {
+        Vector3 directionToFace = _target.position - transform.position;
+        Debug.DrawRay(transform.position, directionToFace, Color.black);
+        // 常時エイム
+        // transform.rotation = Quaternion.LookRotation(directionToFace);
+        // ゆっくりエイム
+        Quaternion targetRotation = Quaternion.LookRotation(directionToFace);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime);
+    }
+}
