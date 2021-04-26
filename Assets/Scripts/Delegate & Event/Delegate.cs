484dc80cf28eb50462d1251ec4953b05306ff1a8
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-26 13:49:06 +0900

    基本: Delegete

---
+++ Assets/Scripts/Delegate & Event/Delegate.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delegate : MonoBehaviour
+{
+    // デリゲート宣言 void型 引数あり
+    public delegate void ChangeColor(Color newColor);
+    // 上記で宣言したデリゲートのインスタンス変数
+    public ChangeColor onColorChange;
+
+    void Start()
+    {
+        onColorChange = UpdateColor; // 格納
+        onColorChange(Color.green);  // 実行
+    }
+
+    // 引数に Color を要求するメソッド
+    void UpdateColor(Color newColor)
+    {
+        Debug.Log("Change Color to" + newColor.ToString());
+    }
+}
