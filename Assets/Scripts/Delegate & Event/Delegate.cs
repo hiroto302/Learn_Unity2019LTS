b6c41f52f2bdb1fa65eed80b29fd67b14492790b
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-26 13:59:30 +0900

    引数をとるDelegate

--- Assets/Scripts/Delegate & Event/Delegate.cs
+++ Assets/Scripts/Delegate & Event/Delegate.cs
@@ -6,18 +6,33 @@ public class Delegate : MonoBehaviour
 {
     // デリゲート宣言 void型 引数あり
     public delegate void ChangeColor(Color newColor);
-    // 上記で宣言したデリゲートのインスタンス変数
     public ChangeColor onColorChange;
+
+    // Delegate ReturnType
+    public delegate int CharacterLength(string text);
+    CharacterLength c1;
+
+    //
 
     void Start()
     {
         onColorChange = UpdateColor; // 格納
         onColorChange(Color.green);  // 実行
+
+        c1 = GetCharacters;
+        Debug.Log(c1("HIRO302") + " : CharacterLength");
+
+
     }
 
     // 引数に Color を要求するメソッド
     void UpdateColor(Color newColor)
     {
         Debug.Log("Change Color to" + newColor.ToString());
+    }
+    // Return Type int
+    int GetCharacters(string name)
+    {
+        return name.Length;
     }
 }
