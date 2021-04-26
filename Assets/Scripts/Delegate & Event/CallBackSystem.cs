08b0d95ae11251e9ffc557e39db5ed543fe849d0
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-26 20:51:55 +0900

    CallBackSystem 一定時間経過後に行いたい処理を記述できる！！

---
+++ Assets/Scripts/Delegate & Event/CallBackSystem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class CallBackSystem : MonoBehaviour
+{
+    void Start()
+    {
+        Action action1 = () => Debug.Log("My Action1");
+
+        StartCoroutine(MyRoutine(action1));
+
+        StartCoroutine(MyRoutine(() => {
+            Debug.Log("The routine has finished");
+        }));
+    }
+
+    // 一定時間後に引数のActionを実行するコールバック
+    public IEnumerator MyRoutine(Action onComplet = null)
+    {
+        yield return new WaitForSeconds(3.0f);
+        if(onComplet != null)
+        {
+            onComplet();
+        }
+    }
+}
