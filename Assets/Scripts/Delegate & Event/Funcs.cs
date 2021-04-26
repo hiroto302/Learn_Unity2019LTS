8d387e83184f37b35226482f8b23547b5d379ad8
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-26 16:07:50 +0900

    Func Lambda

---
+++ Assets/Scripts/Delegate & Event/Funcs.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Funcs : MonoBehaviour
+{
+    // <引数, 返り値>
+    public Func<string, int> func1;
+    // 引数なし <返り値>
+    public Func<int> func2;
+
+    void Start()
+    {
+        // Lambda
+        // (引数) => 返り値
+        func1 = (name) => name.Length;
+        Debug.Log(func1("HIRO") + ": func1 name.Length");
+        // 引数なし
+        func2 = () => 777;
+        Debug.Log("今日の運勢 : " + func2());
+    }
+}
