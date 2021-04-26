84c2c33e03ef32f6e805f42a6756e2139164415a
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-26 17:04:04 +0900

    Delegate Event について

---
+++ Assets/Scripts/Delegate & Event/God.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class God : MonoBehaviour
+{
+    void OnEnable()
+    {
+        Human.onDeath += Rebirth;
+    }
+
+    void Rebirth(Human human)
+    {
+        Debug.Log("転生の時じゃ");
+        human.age = 0;
+        human.gameObject.GetComponent<MeshRenderer>().material.color = new Color(Random.value, Random.value, Random.value);
+    }
+
+    void Start()
+    {
+        // onDeath は event なので God からは実行できない
+        // Human.onDeath();
+    }
+}
---
+++ Assets/Scripts/Delegate & Event/Human.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+// event について
+public class Human : MonoBehaviour
+{
+    // Human が死んだときの処理を格納
+    // 死のイベントが発生した時に、他のクラスが 死んだ Human にどのような処理を行うか記述できるようにするための delegate
+    public delegate void OnDeath(Human human);
+    // public static event にすることで, 他のクラスでも onDeath に処理を追加できるが、処理を実行することはできないようにする
+    public static event OnDeath onDeath;
+
+    // 年齢
+    public int age = 70;
+
+    void Update()
+    {
+        if(Input.anyKeyDown)
+        {
+            age += 10;
+            if(age >= 100)
+            {
+                // 100を超えたら死ぬ
+                Death();
+            }
+        }
+    }
+
+    void Death()
+    {
+        // 死んだ時の event 発生
+        onDeath(this);
+    }
+}
