e9a56f761b1676ff6cab18ca110c0a70851d6c65
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-26 19:16:10 +0900

    delegate event 活用例

---
+++ Assets/Scripts/Delegate & Event/AddEvents.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddEvents : MonoBehaviour
+{
+    // even tを追加するクラス
+
+    void Start()
+    {
+        Events.actionClick += AddActionClick;
+    }
+
+    public void AddActionClick()
+    {
+        Debug.Log("追加されたのじゃ");
+    }
+}
---
+++ Assets/Scripts/Delegate & Event/Events.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*  lear Events : This is a powerful system that calls functions from one class to another without a direct link.
+    other class can invoke and control the delegate while you can only subscribe or unsubscribe a method to an event.
+    他のクラスでイベント内容を決めることができるが,イベントを実行制御をするのはこのクラスのみ */
+public class Events : MonoBehaviour
+{
+    // クリックした時に、実行する event
+    public delegate void ActionClick();
+    public static event ActionClick actionClick;
+
+    void Update()
+    {
+        ButtonClick();
+    }
+
+    public void ButtonClick()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            // NullReferenceException: Object reference not set to an instance of an object の Error を出さないための記述
+            if(actionClick != null)
+            {
+                actionClick();
+            }
+        }
+    }
+}
+
